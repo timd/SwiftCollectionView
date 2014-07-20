ed9fc231e97591b8c68d20f1adfa198db44f103e
Tim Duckett <tim@timduckett.co.uk>
2014-07-20 12:30:29 +0200

    Initial commit

--- SwiftCollectionView/ViewController.swift
+++ SwiftCollectionView/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    var collectionViewData = String[]()
+    
+    let cellIdentifier = "cellIdentifier"
+    
+    @IBOutlet var collectionView: UICollectionView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Setup collection view data
+        self.setupData()
+        
+        // Configure collection view
+        self.configureCollectionView()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Configuration methods
+    
+    func setupData() {
+        for index in 0...200 {
+            collectionViewData += "Item \(index)"
+        }
+    }
+    
+    func configureCollectionView() {
+        
+        var nibName = UINib(nibName: "CollectionViewCell", bundle: nil)
+        self.collectionView.registerNib(nibName, forCellWithReuseIdentifier: self.cellIdentifier)
+        
+        var flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSizeMake(100.0, 100.0)
+        flowLayout.minimumInteritemSpacing = 10.0
+        flowLayout.minimumLineSpacing = 10
+        flowLayout.scrollDirection = UICollectionViewScrollDirection.Vertical
+        
+        self.collectionView.collectionViewLayout = flowLayout
+        self.collectionView.contentInset = UIEdgeInsets(top: 0, left: 10.0, bottom: 10.0, right: 10.0)
+        
+    }
+    
+    // UICollectionViewDatasource methods
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView!) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int {
+        return self.collectionViewData.count
+    }
+
+    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
+        
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(self.cellIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+        var label:UILabel = cell.viewWithTag(999) as UILabel
+        label.text = self.collectionViewData[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    // UICollectionViewDelegate methods
+    func collectionView(collectionView: UICollectionView!, didSelectItemAtIndexPath indexPath: NSIndexPath!) {
+
+        var alert = UIAlertController(title: "Item selected", message: "Selected item \(indexPath.row)", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
 
 }
 
